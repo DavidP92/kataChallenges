545d01d0abd8630a5b490f07daa627038163a3bb
MaSanto Raffiki <Davidpaez1006@gmail.com>
2019-03-03 00:43:19 -0700

    Create invertListValues.py

---
+++ invertListValues.py
@@ -0,0 +1,12 @@
+##Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+##invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+##invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+##invert([]) == []
+##You can assume that all values are integers.
+
+
+
+def invert(list):
+  for x in range(len(list)):
+      list[x] = -list[x]
+  return list
